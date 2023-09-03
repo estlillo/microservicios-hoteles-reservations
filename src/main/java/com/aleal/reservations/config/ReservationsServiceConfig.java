56cb8f62792557353086a88a11cfbe0beaaf95fe
Esteban Lillo <estebanlilloc@gmail.com>
2023-09-03 18:57:02 -0300

    setup config properties from configServer

---
+++ src/main/java/com/aleal/reservations/config/ReservationsServiceConfig.java
@@ -0,0 +1,19 @@
+package com.aleal.reservations.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration // Enable Spring Cloud Config Server test 1
+@ConfigurationProperties(prefix = "reservations")
+@Getter
+@Setter
+public class ReservationsServiceConfig {
+
+    private String msg;
+    private String buildVersion;
+    private Map<String, String> mailDetails;
+}
--- src/main/java/com/aleal/reservations/controller/ReservationController.java
+++ src/main/java/com/aleal/reservations/controller/ReservationController.java
@@ -2,6 +2,11 @@ package com.aleal.reservations.controller;
 
 import java.util.List;
 
+import com.aleal.reservations.config.ReservationsServiceConfig;
+import com.aleal.reservations.model.PropertiesReservations;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +19,22 @@ public class ReservationController {
 	
 	@Autowired
 	private IReservationService service;
-	
+
+	@Autowired
+	private ReservationsServiceConfig config;
 
 	@GetMapping("reservations")
 	public List<Reservation> search(){
 		return (List<Reservation>) this.service.search();	
 	}
 
+
+	@GetMapping("reservations/properties")
+	public String getProperties() throws JsonProcessingException {
+
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		PropertiesReservations properties = new PropertiesReservations(config.getMsg(), config.getBuildVersion(), config.getMailDetails());
+
+		return ow.writeValueAsString(properties);
+	}
 }
